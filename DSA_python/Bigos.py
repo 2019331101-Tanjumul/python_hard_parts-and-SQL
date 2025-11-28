7a69e9151ee8f7e8e77ca16d7337fbf5e6f11041
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-11-28 22:06:55 +0600

    O(1)

---
+++ DSA_python/Bigos.py
@@ -0,0 +1,5 @@
+#big O of O(1) , no matters how much the input it take the order / function here is simple
+def multily_num (n): 
+    return n*n
+
+print(multily_num(2))
---
+++ Sql/foregin_and_primary_key.sql
@@ -0,0 +1,24 @@
+--table 1 
+
+create table customers (
+
+cust_id serial PRIMARY KEY,
+fanme VARCHAR(100) not null
+);
+
+insert into customers (fname) values ('tanjumul'), ('tanvir') , ('garal'),('gauro');
+
+--table 2
+
+create table orders ( 
+
+ord_id serial primary key,
+ord_date date not null ,
+price NUMERIC not null , 
+cust_id integer not null, 
+
+Foreign Key (cust_id) REFERENCES customers (cust_id)
+
+);
+
+insert into orders (cust_id,ord_date,price) values ('1','2025-01-24',260),('2','2025-01-24',260),('1','2025-01-24',260),('3','2025-01-24',260),('2','2025-01-24',460),('1','2025-01-24',760),('3','2025-01-24',660),('4','2025-01-24',360);
