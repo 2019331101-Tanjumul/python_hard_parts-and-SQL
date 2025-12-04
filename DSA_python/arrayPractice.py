5fcc0df985195b56e661b1a0daec38ca0edafaf9
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-12-04 19:59:39 +0600

    Solution no . 1 & 2

---
+++ DSA_python/arrayPractice.py
@@ -0,0 +1,34 @@
+"""
+Here some of the questions that will be solved : 
+1. Create an array and traverse . 
+2. Access individual elements through indexes. 
+3.append any value to the arrray using append() method
+4.Insert values in an array using insert() method 
+5.Extend python array using extend method 
+6.add items from list into array using fromlist()
+7.Remove last array element using pop()
+8.fetch any element through its index using index() method 
+10.Reverse a ython array using reverse method 
+11.get array buffer information through buffer_info() method 
+12.Check for number of occurrences of an element using count() method 
+13.Convert array to string using tostring method 
+14.convert arrray to a pythonn list with same elements using todolist method 
+15.append a string to char array using fromstring() method 
+16.slice element from an array.
+"""
+
+from array import * 
+# 1. Create an array and traverse . 
+# my_array = array('i',[1,2,33,44,55,6])
+
+# for i in my_array: 
+#     print(i)
+
+
+# 2. Access individual elements through indexes. 
+
+print("Question no : 2")
+arr1 = array('i',[2,12,44,25,66,43,2])
+
+# for i in range(len(arr1)): 
+print(arr1[3])
