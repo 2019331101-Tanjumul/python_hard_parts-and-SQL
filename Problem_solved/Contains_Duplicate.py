74ddd4f48aa422849b4fa4630c0f245bb4876346
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-12-06 19:53:20 +0600

    quick note of the solution

--- Problem_solved/Contains_Duplicate.py
+++ Problem_solved/Contains_Duplicate.py
@@ -7,3 +7,21 @@ class Solution:
             if nums[i] == nums[i-1]:
                 return True
         return False
+
+
+
+
+
+
+
+
+
+"""Nothing here fancy : 
+here the nums is as a list now : 
+i don't know how the numbers are formed inside the list , 
+i will sort it first of the messy like numbers can be alligned like Example : 
+Before : nums[12,1,44,2,6,4,3,0,6]
+After : nums[0,1,2,3,4,6,6,12,44]
+
+now i , looped form 1 to length of the array , some time without 1 it give your a true result which is wrong. 
+after that you do a condition check weather the index of nums is matching with it's previous index , if matching `return True not in 'true' form ` without it can give you error. thta's it break the loop and add your next return."""
