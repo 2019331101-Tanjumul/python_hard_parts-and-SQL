7b72bd2adb9e3565f2271b3e177166333a570b91
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-12-06 18:31:49 +0600

    deleting the whole row in the numpy array

--- DSA_python/delete_arrays.py
+++ DSA_python/delete_arrays.py
@@ -2,6 +2,10 @@ import numpy as np
 
 mynew_array = np.array([[81,82,83,84],[71,72,73,74],[61,62,63,64],[11,22,33,44]])
 
+mynew_array = np.delete(mynew_array,0,axis=0)
+print(mynew_array)
+
+
 # def deletearray(array,delNum): 
 #     arr_list = array.tolist()
 #     found = False
@@ -14,14 +18,15 @@ mynew_array = np.array([[81,82,83,84],[71,72,73,74],[61,62,63,64],[11,22,33,44]]
 
 # print(deletearray(mynew_array,61))
      
-     #python list alternative version of numpy   
-def deletearray(array, delNum): 
-    arr_list = array.tolist()
-    for i in range(len(arr_list)): 
-        for j in range(len(arr_list[0])):
-            if arr_list[i][j] == delNum: 
-                arr_list[i].pop(j)
-                return np.array(arr_list)
-    print('Number not found')
-    return array
-print(deletearray(mynew_array ,61))
+     #python list alternative version of    
+# def deletearray(array, delNum): 
+#     arr_list = array.tolist()
+#     for i in range(len(arr_list)): 
+#         for j in range(len(arr_list[0])):
+#             if arr_list[i][j] == delNum: 
+#                 arr_list[i].pop(j)
+#                 return np.array(arr_list)
+#     print('Number not found')
+#     return array
+# print(deletearray(mynew_array ,61))
+
